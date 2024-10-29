7c57845aca58049bf6d81e3b823c24248f0b3266
saket kumar keshri <keshrisaket414@gmail.com>
2024-10-29 23:05:43 +0530

    first commit

---
+++ src/main/java/com/movieapis/MovieApisApplication.java
@@ -0,0 +1,14 @@
+package com.movieapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApisApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MovieApisApplication.class, args);
+    }
+
+}
